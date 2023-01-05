aa4dc7fcba2f00960b372a15c89cf47ed0229fda
leonardo scommegna <leonardo.scommegna@unifi.com>
2023-01-05 11:25:57 +0100

    License notices added

--- src/main/java/org/oristool/sumo/plotter/Plotter.java
+++ src/main/java/org/oristool/sumo/plotter/Plotter.java
@@ -1,3 +1,17 @@
+/********************************************************************************
+ * This program is part of a software application using SUMO
+ * (Simulation of Urban MObility, see https://eclipse.org/sumo)
+ * to analyze multimodal urban intersections.
+ * 
+ * Copyright (C) 2022-2023 Software Technologies Lab, University of Florence. 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
 package org.oristool.sumo.plotter;
 
 import org.jfree.chart.ChartFactory;
